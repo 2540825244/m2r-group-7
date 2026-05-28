971e4dd8273d146f9929501a7c315caa2495b85d
Sevin Fernando <saf224@ic.ac.uk>
2026-05-28 13:13:03 +0100

    feat: prime_cubed_non_abelian up to claim 9 [saf224, claude]

--- M2rGroup7/Classification.lean
+++ M2rGroup7/Classification.lean
@@ -21,8 +21,20 @@ import Mathlib.GroupTheory.PGroup
 import OrderPQ
 import Mathlib.Algebra.Group.Defs
 import Mathlib.GroupTheory.Commutator.Basic
+import Mathlib.Algebra.Group.Equiv.Defs
+import Mathlib.Algebra.Group.Subgroup.ZPowers.Basic
+import Mathlib.Algebra.Group.TypeTags.Basic
+import Mathlib.GroupTheory.Commutator.Basic
+import Mathlib.Data.Bracket
+import Mathlib.Algebra.Group.TypeTags.Basic
 
 def maximumOrder : Nat := 9
+
+open scoped commutatorElement
+
+lemma AddSubgroup.closure_singleton_int_one_eq_top : closure ({1} : Set ℤ) = ⊤ := by
+  ext
+  simp [mem_closure_singleton]
 
 variable (n : ℕ) (G : Type*) [Group G]
 
@@ -39,20 +51,25 @@ lemma isMulCommutative_of_mulEquiv {M N : Type*} [Group M] [Group N] (e : M ≃*
 theorem center_eq_top_iff : Subgroup.center G = ⊤ ↔ IsMulCommutative G := by
   simp [Subgroup.eq_top_iff', isMulCommutative_iff, Subgroup.mem_center_iff, eq_comm]
 
+theorem prime_classification [hn : Fact n.Prime] (h : Nat.card G = n) :
+(Nonempty (MulEquiv G (CyclicGroup n))) := by
+  apply Nonempty.intro
+  have h_g_card : Nat.card G = n := h
+  have : IsCyclic G := isCyclic_of_prime_card h_g_card
+  refine (mulEquivOfCyclicCardEq ?_)
+  have h_c_card: Nat.card (CyclicGroup n) = n := card_cyclicGroup n
+  rw [h_g_card, h_c_card]
+
 theorem prime_cubed_non_abelian_classification {p : ℕ} [hn : Fact p.Prime] (h_na : ¬IsMulCommutative G) (h : Nat.card G = p^3) :
   True := by
-  -- Define Z as the center of G
   set Z := Subgroup.center G with hZ
   -- Claim 1: |Z(G)| = p and so Z(G) isomorphic to C_p
-  -- Step 1: Z(G) subgroup of G implies |Z(G)| in {1, p, p^2, p^3}
   have h_z_order_divides_p_3 := by
     apply Subgroup.card_subgroup_dvd_card Z
   rw [h] at h_z_order_divides_p_3
   rw [dvd_prime_pow hn.out.prime] at h_z_order_divides_p_3
   obtain ⟨i, h'⟩ := h_z_order_divides_p_3
   obtain ⟨h_bound, h_z_order_p_i⟩ := h'
-
-  -- Step 2: |Z(G)| ≠ 1 as centers of p-groups are non-trivial
 
   have : Finite G := by
     apply Nat.finite_of_card_ne_zero
@@ -84,8 +101,6 @@ theorem prime_cubed_non_abelian_classification {p : ℕ} [hn : Fact p.Prime] (h_
     obtain ⟨h_ord_one, _⟩ := hu
     omega
 
-  -- Step 3: |Z(G)| ≠ p^3 as then Z(G) isomorphic to G which is iff G is abelian, contradiction
-
   have h_z_card_ne_p3 : Nat.card ↥Z ≠ (p^3) := by
     intro h_z_p3
     have h_z_eq_g : Z = ⊤ := by
@@ -94,8 +109,6 @@ theorem prime_cubed_non_abelian_classification {p : ℕ} [hn : Fact p.Prime] (h_
       rw [← center_eq_top_iff, ← hZ]
       exact h_z_eq_g
     exact absurd h_g_abelian h_na
-
-  -- Step 4: |Z(G)| ≠ p^2 as then |G/Z(G)| = p and so it is cyclic so G is abelian, contradiction
 
   have h_z_card_ne_p2 : Nat.card ↥Z ≠ (p^2) := by
     intro h_z_p2
@@ -120,9 +133,6 @@ theorem prime_cubed_non_abelian_classification {p : ℕ} [hn : Fact p.Prime] (h_
     have h_ker_subgroup_z : (QuotientGroup.mk' Z).ker ≤ Z := by
       rw [QuotientGroup.ker_mk' Z]
 
-    -- have h_g_comm : CommGroup G := by
-    --   exact commGroupOfCyclicCenterQuotient (QuotientGroup.mk' Z) (h_ker_subgroup_z)
-
     have h_comm := commutative_of_cyclic_center_quotient (QuotientGroup.mk' Z) (h_ker_subgroup_z)
 
     contrapose! h_na
@@ -142,10 +152,7 @@ theorem prime_cubed_non_abelian_classification {p : ℕ} [hn : Fact p.Prime] (h_
     have hi1 : i = 1 := by omega
     rw [h_card_eq, hi1, pow_one]
 
-
   -- Claim 2: G / Z is C_p x C_p
-
-  -- Step 1: G / Z is order p^2
 
   have h_z_idx_p2 : Z.index = p^2 := by
     have h_index_mul_card := Subgroup.index_mul_card Z
@@ -159,9 +166,6 @@ theorem prime_cubed_non_abelian_classification {p : ℕ} [hn : Fact p.Prime] (h_
 
   have h_g_quot_z_classify := p_squared_classification h_g_quot_z_p_card
 
-  -- Step 2: G / Z cannot be cyclic because G is not abelian
-  -- Step 3: Hence G / Z is C_p x C_p
-
   have h_g_quot_z_is_Cp_x_Cp : Nonempty (MulEquiv (G ⧸ Z) (CyclicGroup p × CyclicGroup p)) := by
     cases h_g_quot_z_classify with
       | inl h_g_quot_z_is_Cp2 =>
@@ -170,14 +174,12 @@ theorem prime_cubed_non_abelian_classification {p : ℕ} [hn : Fact p.Prime] (h_
           let iso := Classical.choice h_g_quot_z_is_Cp2
           rw [MulEquiv.isCyclic iso]
           infer_instance
-
         sorry
+
       | inr hb =>
         exact hb
 
   -- Claim 3: [G, G] = Z(G)
-
-  -- Step 1: Z(G) contains [G, G] as Z(G) is normal and G/N abelian iff N contains [G, G]
 
   have h_quotient_z_abelian : IsMulCommutative (G ⧸ Z) := by
     obtain ⟨e⟩ := h_g_quot_z_is_Cp_x_Cp
@@ -188,14 +190,12 @@ theorem prime_cubed_non_abelian_classification {p : ℕ} [hn : Fact p.Prime] (h_
   have h_comm_sub_z : commutator G ≤ Z := by
     exact Subgroup.Normal.quotient_commutative_iff_commutator_le.mp h_quotient_z_abelian.is_comm
 
-  -- Step 2: [G, G] is either trivial group or Z(G) but it is trivial iff G is abelian which is not true
-
   have hcomm_nontrivial : commutator G ≠ ⊥ := by
     intro h
     apply h_na
-    have hcenter : Subgroup.center G = ⊤ := (commutator_eq_bot_iff_center_eq_top G).mp h
+    have h_center : Subgroup.center G = ⊤ := (commutator_eq_bot_iff_center_eq_top G).mp h
     exact IsMulCommutative.mk ⟨fun a b => by
-      have ha : a ∈ Subgroup.center G := hcenter ▸ Subgroup.mem_top a
+      have ha : a ∈ Subgroup.center G := h_center ▸ Subgroup.mem_top a
       symm
       exact (Subgroup.mem_center_iff.mp ha) b⟩
 
@@ -211,71 +211,241 @@ theorem prime_cubed_non_abelian_classification {p : ℕ} [hn : Fact p.Prime] (h_
 
   have h_card_comm : Nat.card (commutator G) = p := h_or.resolve_left h_ne_one
 
-  have hcard_subgroupOf : Nat.card ((commutator G).subgroupOf Z) = Nat.card (commutator G) :=
+  have h_card_subgroupOf : Nat.card ((commutator G).subgroupOf Z) = Nat.card (commutator G) :=
     Nat.card_congr (Subgroup.subgroupOfEquivOfLe h_comm_sub_z).toEquiv
 
   have h_card_comm_z : Nat.card (commutator G) = Nat.card Z := h_card_comm.trans h_z_card_eq_p.symm
+
   have h_comm_top : (commutator G).subgroupOf Z = ⊤ := by
     rw [← Subgroup.card_eq_iff_eq_top]
-    rw [hcard_subgroupOf]
+    rw [h_card_subgroupOf]
     exact h_card_comm_z
 
-  have hcomm_eq_z : commutator G = Z := by
+  have h_comm_eq_z : commutator G = Z := by
     rw [Subgroup.subgroupOf_eq_top] at h_comm_top
     exact le_antisymm h_comm_sub_z h_comm_top
 
-
   -- Claim 4: There exists a, b in G such that aZ, bZ non-identity in G/Z and generate G/Z
 
-  -- Step 1: There exist aZ, bZ that generate G/Z as G/Z is C_p x C_p
+  obtain ⟨x⟩ := h_g_quot_z_is_Cp_x_Cp
 
-  -- Step 2: As G -> G/Z is surjective, claim holds
+  let gen1 : CyclicGroup p × CyclicGroup p := (Multiplicative.ofAdd (1 : ZMod p), 1)
+  let gen2 : CyclicGroup p × CyclicGroup p := (1, Multiplicative.ofAdd (1 : ZMod p))
 
+  let aZ : G ⧸ Z := x.symm gen1
+  let bZ : G ⧸ Z := x.symm gen2
 
+  obtain ⟨a, ha⟩ := QuotientGroup.mk_surjective aZ
+  obtain ⟨b, hb⟩ := QuotientGroup.mk_surjective bZ
+
+  have hgen1_ne : gen1 ≠ 1 := by
+    simp [gen1]
+    intro h
+    have : (1 : ZMod p) = 0 := Multiplicative.ofAdd.injective h
+    exact absurd this one_ne_zero
+
+  have hgen2_ne : gen2 ≠ 1 := by
+    simp [gen2]
+    intro h
+    have : (1 : ZMod p) = 0 := Multiplicative.ofAdd.injective h
+    exact absurd this one_ne_zero
+
+  have haZ_ne : aZ ≠ 1 := by
+    intro h
+    apply hgen1_ne
+    exact x.symm.injective (by simp [aZ, h])
+
+  have hbZ_ne : bZ ≠ 1 := by
+    intro h
+    apply hgen2_ne
+    exact x.symm.injective (by simp [bZ, h])
+
+  have h_hom_a : x (QuotientGroup.mk' Z a) = gen1 := by
+    have hmk : QuotientGroup.mk' Z a = ↑a := rfl
+    rw [hmk, ha]; simp only [aZ]; exact x.apply_symm_apply gen1
+
+  have h_hom_b : x (QuotientGroup.mk' Z b) = gen2 := by
+    have hmk : QuotientGroup.mk' Z b = ↑b := rfl
+    rw [hmk, hb]; simp only [bZ]; exact x.apply_symm_apply gen2
+
+  have hgen_cp : ∀ a : CyclicGroup p, ∃ k : ℤ, (Multiplicative.ofAdd (1 : ZMod p)) ^ k = a := by
+    intro a
+    refine ⟨(Multiplicative.toAdd a).cast, ?_⟩
+    rw [← ofAdd_zsmul, zsmul_one]
+    rw [ZMod.intCast_zmod_cast]
+    rw [ofAdd_toAdd]
+
+  have hprod_gen : ∀ q : CyclicGroup p × CyclicGroup p,
+    ∃ m n : ℤ, q = gen1 ^ m * gen2 ^ n := by
+    intro ⟨a, b⟩
+    obtain ⟨m, hm⟩ := hgen_cp a
+    obtain ⟨n, hn⟩ := hgen_cp b
+    refine ⟨m, n, ?_⟩
+    simp [gen1, gen2, Prod.ext_iff]
+    constructor
+    · exact hm.symm
+    · exact hn.symm
+
+  have hquot_gen : ∀ q : G ⧸ Z, ∃ m n : ℤ, q = aZ ^ m * bZ ^ n := by
+    intro q
+    obtain ⟨m, n, hmn⟩ := hprod_gen (x q)
+    refine ⟨m, n, ?_⟩
+    have := congr_arg x.symm hmn
+    simp [MulEquiv.map_mul, aZ, bZ] at this ⊢
+    exact this
 
   -- Claim 5: Z, a, b generate G
 
-  -- Step 1: Let g in G, gZ is generated by aZ and bZ
-
-  -- Step 2: g z_1 = <a, b> z_2
-
-  -- Step 3: Hence g = <a, b> z_2 z_1^-1
-
-
+  have hG_gen : ∀ g : G, ∃ m n : ℤ, ∃ z ∈ Z, g = a ^ m * b ^ n * z := by
+    intro g
+    obtain ⟨m, n, hmn⟩ := hquot_gen (QuotientGroup.mk' Z g)
+    refine ⟨m, n, ?_⟩
+    simp [aZ, bZ] at hmn
+    change ↑g = aZ ^ m * bZ ^ n at hmn
+    rw [← ha, ← hb] at hmn
+    rw [← QuotientGroup.mk_zpow, ← QuotientGroup.mk_zpow, ← QuotientGroup.mk_mul] at hmn
+    rw [QuotientGroup.eq] at hmn
+    exact ⟨(a ^ m * b ^ n)⁻¹ * g, by rw [← Subgroup.inv_mem_iff]; convert hmn using 1; group, by group⟩
 
   -- Claim 6: a, b do not commute
 
-  -- Step 1: If a, b commute then centraliser C_G(a) contains Z, a, b
-
-  -- Step 2: C_G(a) =* G
-
-  -- Step 3: Hence a in Z but aZ is not identity in G/Z
-
-
+  have hab_comm : a * b ≠ b * a := by
+    intro h_comm
+    have h_b_cent : b ∈ Subgroup.centralizer {a} := by
+      rw [Subgroup.mem_centralizer_iff]
+      intro g hg
+      simp at hg
+      rw [hg]
+      exact h_comm
+    have h_Z_cent : Z ≤ Subgroup.centralizer {a} := by
+      intro z hz
+      rw [Subgroup.mem_centralizer_iff]
+      intro g hg
+      simp at hg
+      rw [hg]
+      exact (Subgroup.mem_center_iff.mp hz) a
+    have h_a_cent : a ∈ Subgroup.centralizer {a} := by
+      rw [Subgroup.mem_centralizer_iff]
+      intro g hg
+      simp at hg
+      rw [hg]
+    have hcent_top : Subgroup.centralizer {a} = ⊤ := by
+      rw [eq_top_iff]
+      intro g _
+      obtain ⟨m, n, z, hz, hgz⟩ := hG_gen g
+      rw [hgz]
+      apply Subgroup.mul_mem
+      · apply Subgroup.mul_mem
+        · exact Subgroup.zpow_mem _ h_a_cent m
+        · exact Subgroup.zpow_mem _ h_b_cent n
+      · exact h_Z_cent hz
+    have ha_in_Z : a ∈ Z := by
+      rw [hZ, Subgroup.mem_center_iff]
+      intro g
+      have hg : g ∈ Subgroup.centralizer {a} := by rw [hcent_top]; exact Subgroup.mem_top g
+      rw [Subgroup.mem_centralizer_iff] at hg
+      exact (hg a rfl).symm
+    have h_aZ_eq_one: aZ = 1 := by
+      rw [← ha]
+      show (↑a : G ⧸ Z) = ↑(1 : G)
+      rw [QuotientGroup.eq]
+      simp [ha_in_Z]
+    exact absurd h_aZ_eq_one haZ_ne
 
   -- Claim 7: [a, b] is a non-identity element and generates Z
 
-  -- Step 1: [a, b] non-identity because a, b do not commute
+  have hab_comm_ne : ⁅a, b⁆ ≠ 1 := by
+    intro h
+    apply hab_comm
+    rwa [commutatorElement_eq_one_iff_mul_comm] at h
 
-  -- Step 2: Z is C_p and so is generated by [a, b] as it is generated by any non-identity element
+  have hab_in_Z : ⁅a, b⁆ ∈ Z := by
+    rw [← h_comm_eq_z, commutator_eq_normalClosure]
+    apply Subgroup.subset_normalClosure
+    exact @commutator_mem_commutatorSet G _ a b
 
-
+  have hab_generates_Z : ∀ z ∈ Z, z ∈ Subgroup.zpowers ⁅a, b⁆ := by
+    intro z hz
+    have hmem := @mem_zpowers_of_prime_card (↥Z) _ p _ h_z_card_eq_p ⟨⁅a, b⁆, hab_in_Z⟩ ⟨z, hz⟩ ?_
+    obtain ⟨k, hk⟩ := Subgroup.mem_zpowers_iff.mp hmem
+    exact Subgroup.mem_zpowers_iff.mpr ⟨k, congr_arg Subtype.val hk⟩
+    intro h
+    apply hab_comm_ne
+    have := congr_arg Subtype.val h
+    simp at this
+    exact this
 
   -- Claim 8: a, b have order either p or p^2
 
-  -- Step 1: a, b have order either 1, p, p^2, p^3
+  have ha_order_dvd : orderOf a ∣ p ^ 3 := by
+    have := orderOf_dvd_natCard a
+    rwa [h] at this
 
-  -- Step 2: a, b not identity so not order 1
+  have ha_order_cases : orderOf a = 1 ∨ orderOf a = p ∨ orderOf a = p ^ 2 ∨ orderOf a = p ^ 3 := by
+    have := (Nat.dvd_prime_pow hn.out).mp ha_order_dvd
+    obtain ⟨i, hi, hia⟩ := this
+    interval_cases i <;> simp_all
 
-  -- Step 3: G is not abelian, so not cyclic, so a, b not order p^3
+  have ha_order_ne_one : orderOf a ≠ 1 := by
+    intro h
+    apply haZ_ne
+    rw [← ha]
+    have : a = 1 := orderOf_eq_one_iff.mp h
+    simp [this]
 
+  have ha_order_ne_three : orderOf a ≠ (p ^ 3) := by
+    intro h_order_eq_three
+    rw [← h] at h_order_eq_three
+    apply Eq.ge at h_order_eq_three
+    have h_cyc : IsCyclic G := isCyclic_of_card_le_orderOf a h_order_eq_three
+    exact h_na (isMulCommutative_iff.mpr (fun x y => h_cyc.commutative.comm x y))
 
+  have ha_order : orderOf a = p ∨ orderOf a = p ^ 2 := by
+    rcases ha_order_cases with h_1 | h_p | h_p2 | h_p3
+    · exact absurd h_1 ha_order_ne_one
+    · exact Or.inl h_p
+    · exact Or.inr h_p2
+    · exact absurd h_p3 ha_order_ne_three
 
   -- Claim 9: a^p, b^p in Z
 
-  -- Step 1: G/Z is C_p x C_p so for every element g, (gZ)^p = Z
+  have hCp_exp : ∀ c : CyclicGroup p, c ^ p = 1 := by
+    intro c
+    haveI : Finite (CyclicGroup p) := by
+      rw [show CyclicGroup p = Multiplicative (ZMod p) from rfl]
+      exact instFiniteMultiplicative
+    haveI : Fintype (CyclicGroup p) := Fintype.ofFinite _
+    have h_card : Nat.card (CyclicGroup p) = p := card_cyclicGroup p
+    have := @pow_card_eq_one (CyclicGroup p) _ _ c
+    rwa [← Nat.card_eq_fintype_card, h_card] at this
 
-  -- Step 2: a^p, b^p in Z
+  have hCp2_exp : ∀ q : CyclicGroup p × CyclicGroup p, q ^ p = 1 := by
+    intro ⟨c, d⟩
+    simp [hCp_exp c, hCp_exp d]
+
+  have hquot_exp : ∀ q : G ⧸ Z, q ^ p = 1 := by
+    intro q
+    have := hCp2_exp (x q)
+    rw [← map_pow] at this
+    exact x.injective (by rwa [map_one])
+
+  have ha_pow_in_Z : a ^ p ∈ Z := by
+    have hmk := hquot_exp (QuotientGroup.mk' Z a)
+    rw [← map_pow] at hmk
+    change ((a ^ p) : G ⧸ Z) = 1 at hmk
+    rw [show (↑a : G ⧸ Z) ^ p = ↑(a ^ p) from (map_pow (QuotientGroup.mk' Z) a p).symm] at hmk
+    rw [show (1 : G ⧸ Z) = ↑(1 : G) from by simp] at hmk
+    rw [QuotientGroup.eq] at hmk
+    simpa using hmk
+
+  have hb_pow_in_Z : b ^ p ∈ Z := by
+    have hmk := hquot_exp (QuotientGroup.mk' Z b)
+    rw [← map_pow] at hmk
+    change (↑b : G ⧸ Z) ^ p = 1 at hmk
+    rw [show (↑b : G ⧸ Z) ^ p = ↑(b ^ p) from (map_pow (QuotientGroup.mk' Z) b p).symm] at hmk
+    rw [show (1 : G ⧸ Z) = ↑(1 : G) from by simp] at hmk
+    rw [QuotientGroup.eq] at hmk
+    simpa using hmk
 
 theorem prime_cubed_and_abelian_classification {p : ℕ} [hn : Fact p.Prime] [CommGroup G] (h : Nat.card G = p^3) :
   (Nonempty (MulEquiv G (CyclicGroup (p^3)))) ∨
@@ -308,15 +478,6 @@ theorem prime_cubed_and_abelian_classification {p : ℕ} [hn : Fact p.Prime] [Co
       sorry
 
     -- have := Multiset.map (p^3) Finset.univ
-
-theorem prime_classification [hn : Fact n.Prime] (h : Nat.card G = n) :
-(Nonempty (MulEquiv G (CyclicGroup n))) := by
-  apply Nonempty.intro
-  have h_g_card : Nat.card G = n := h
-  have : IsCyclic G := isCyclic_of_prime_card h_g_card
-  refine (mulEquivOfCyclicCardEq ?_)
-  have h_c_card: Nat.card (CyclicGroup n) = n := card_cyclicGroup n
-  rw [h_g_card, h_c_card]
 
 macro "classify_prime" p:num h:term : tactic => `(tactic|(
   have : Fact (Nat.Prime $p) := ⟨by decide⟩
