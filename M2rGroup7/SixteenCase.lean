3a9e9a9da64eb65ebf5a2c35460ec190a1750f61
Jay Lees-Maffei <jay.lees-maffei24@imperial.ac.uk>
2026-06-01 12:23:46 +0100

    feat(Order 16): added fact 1 from Wild's paper

--- M2rGroup7/SixteenCase.lean
+++ M2rGroup7/SixteenCase.lean
@@ -4,6 +4,23 @@ import Mathlib
 
 namespace OrderSixteen
 
+/-- Isomorphism for Wild's Fact 1. -/
+noncomputable def fact1_mulEquiv
+    {G : Type*} [Group G] (H₁ H₂ : Subgroup G)
+    (h_disj : H₁ ⊓ H₂ = ⊥)
+    (h_comm : ∀ x ∈ H₁, ∀ y ∈ H₂, x * y = y * x) :
+    (H₁ × H₂) ≃* ↑(H₁ ⊔ H₂) :=
+  sorry
+
+/-- Wild's Fact 1: If `H₁` and `H₂` are subgroups of G such that `H₁ ⊓ H₂ = {e}`
+    and `∀ x ∈ H₁, ∀ y ∈ H₂, xy = yx`,
+    then `H₁H₂` is a subgroup isomorphic to the direct product  `H₁ × H₂`. -/
+lemma fact1 {G : Type*} [Group G] (H₁ H₂ : Subgroup G)
+    (h_disj : H₁ ⊓ H₂ = ⊥)
+    (h_comm : ∀ x ∈ H₁, ∀ y ∈ H₂, x * y = y * x) :
+    Nonempty ((H₁ × H₂ ≃* ↑(H₁ ⊔ H₂)) :=
+  ⟨fact1_mulEquiv H₁ H₂ h_disj h_comm⟩
+
 structure ExtensionType where
   N : Type*
   [g : Group N]
