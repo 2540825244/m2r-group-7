9620dc89e742045649e5d889454a2bad345b8c98
Jay Lees-Maffei <jay.lees-maffei24@imperial.ac.uk>
2026-06-03 13:01:21 +0100

    feat(24 case): implemented proof structure for classification

--- M2rGroup7/TwentyFourCase.lean
+++ M2rGroup7/TwentyFourCase.lean
@@ -1,6 +1,25 @@
 import Mathlib.GroupTheory.Sylow
 
-/-- A group of order `24` is isomorphic to some group. -/
-theorem order24_classification {G: Type*} [Group G] (h : Nat.card G = 24) :
-    True := by
+/-- A group of order `24` has either `1` or `4` Sylow 3-subgroups. -/
+lemma sylow3_24 {G : Type*} [Group G] (h : Nat.card G = 24) :
+    Nat.card (Sylow 3 G) = 1 ∨ Nat.card (Sylow 3 G) = 4 := by
   sorry
+
+/-- A group of order `24` with a unique Sylow 3-subgroup is isomorphic to some group.
+    The precondition is equivalent to having a normal Sylow 3-subgroup. -/
+lemma order24_1_sylow3 {G : Type*} [Group G] (h : Nat.card G = 24)
+    (h_n3 : Nat.card (Sylow 3 G) = 1) : True := by
+  sorry
+
+/-- A group of order `24` with four Sylow 3-subgroups is isomorphic to some group.
+    The precondition is equivalent to not having a normal Sylow 3-subgroup. -/
+lemma order24_4_sylow3 {G : Type*} [Group G] (h : Nat.card G = 24)
+    (h_n3 : Nat.card (Sylow 3 G) = 4) : True := by
+  sorry
+
+/-- A group of order `24` is isomorphic to some group. -/
+theorem order24_classification {G : Type*} [Group G] (h : Nat.card G = 24) :
+    True := by
+  rcases sylow3_24 h with h_n3_1 | h_n3_4
+  · exact order24_1_sylow3 h h_n3_1
+  · exact order24_4_sylow3 h h_n3_4
