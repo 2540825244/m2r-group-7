966c87c43f5663dd2d1746ee92d2e6365cb7ad26
Jay Lees-Maffei <jay.lees-maffei24@imperial.ac.uk>
2026-06-03 12:24:23 +0100

    chore(24 case): fixed typos

--- M2rGroup7/TwentyFourCase.lean
+++ M2rGroup7/TwentyFourCase.lean
@@ -1,6 +1,6 @@
-import Mathlib
+import Mathlib.GroupTheory.Sylow
 
 /-- A group of order `24` is isomorphic to some group. -/
-theorem order24_classification {G: Type*} [Group G] (h : Nat.Card G = 24) :
-    true := by
+theorem order24_classification {G: Type*} [Group G] (h : Nat.card G = 24) :
+    True := by
   sorry
